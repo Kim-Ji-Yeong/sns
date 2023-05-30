e8865616b6f7f474b298c02dc6aaab9607c15628
Kim-Ji-Yeong <yeong6415@daum.net>
2023-05-30 13:46:15 +0900

    service

---
+++ src/main/java/com/example/sns/producer/AlarmProducer.java
@@ -0,0 +1,22 @@
+package com.example.sns.producer;
+
+import com.example.sns.model.event.AlarmEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AlarmProducer {
+
+    private final KafkaTemplate<Integer, AlarmEvent> kafkaTemplate;
+    @Value("${spring.kafka.topic.alarm}")
+    private String topic;
+
+    public void send(AlarmEvent event){
+        kafkaTemplate.send(topic,event.getReceiveUserid(),event);
+        log.info("Send to Kafka finished");
+    }
+}
